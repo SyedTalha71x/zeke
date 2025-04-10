00edc41d7b2c0f7c125c209dd5a19b2699f15a72
SyedTalha71x <syedtalha71x@gmail.com>
2025-04-10 14:47:47 +0500

    update server

--- server/server.js
+++ server/server.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', UserRoutes)
 
-app.get('/', (res)=>{
-    res.send('Server is running')
-})
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
